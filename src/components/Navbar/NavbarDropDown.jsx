7142bd95822f8bd1f27ba47032049ec4c66d6ab6
miguelraad28 <miguelraad2020@gmail.com>
2022-09-06 15:04:44 -0300

    .

--- src/components/Navbar/NavbarDropDown.jsx
+++ src/components/Navbar/NavbarDropDown.jsx
@@ -10,7 +10,7 @@ const NavbarDropDown = () => {
                 <li><hr className="dropdown-divider" /></li>
                 <NavItemDropDown to={"/productos/mazos"} text={"MAZOS"}/>
                 <NavItemDropDown to={"/productos/velas"} text={"VELAS"}/>
-                <NavItemDropDown to={"/productos/limpieza"} text={"LIMPIEZA"}/>
+                <NavItemDropDown to={"/productos/limpieza-energetica"} text={"LIMPIEZA"}/>
                 <NavItemDropDown to={"/productos/tarot"} text={"TAROT"}/>
             </ul>
         </li>
