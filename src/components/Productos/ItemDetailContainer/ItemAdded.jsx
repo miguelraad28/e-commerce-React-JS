d4501293b89e7292ab313f3578fe6c8c56f9835b
miguelraad28 <miguelraad2020@gmail.com>
2022-09-12 23:59:16 -0300

    .

--- src/components/Productos/ItemDetailContainer/ItemAdded.jsx
+++ src/components/Productos/ItemDetailContainer/ItemAdded.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./ItemAdded.scss"
-const ItemAdded = () => {
+const ItemAdded = ({set}) => {
     return (
         <div className="productoAgregado">
             <h3>Producto agregado</h3>
+            <button onClick={() => set(true)}>Modificar cantidad</button>
             <Link to="/cart">
                 <button>Finalizar compra</button>
             </Link>
--- src/components/Productos/ItemDetailContainer/ItemDetail.jsx
+++ src/components/Productos/ItemDetailContainer/ItemDetail.jsx
@@ -5,9 +5,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ItemAdded from "./ItemAdded"
 const ItemDetail = ({id, nombre, descripcion, precioUnidad, stock, img}) => {
-    const [cambiarComponente, setCambiarComponente] = useState(true);
+    const [productoAgregado, setProductoAgregado] = useState(true);
     function onAdd(count){
-        setCambiarComponente(false)
+        setProductoAgregado(false)
         if(count < 1){
             toast.error('Debes añadir al menos 1', {
                 position: "top-right",
@@ -41,7 +41,7 @@ const ItemDetail = ({id, nombre, descripcion, precioUnidad, stock, img}) => {
                     <p className='itemDescripcion'>{descripcion}</p>
                     <p>Precio: ${precioUnidad}</p>
                     <p>Disponibles: {stock}</p>
-                    {cambiarComponente ? <ItemCount stock={stock} onAdd={onAdd}/> : <ItemAdded/>}
+                    {productoAgregado ? <ItemCount stock={stock} onAdd={onAdd}/> : <ItemAdded set={setProductoAgregado}/>}
                     <ToastContainer
                     position="top-right"
                     autoClose={1700}
